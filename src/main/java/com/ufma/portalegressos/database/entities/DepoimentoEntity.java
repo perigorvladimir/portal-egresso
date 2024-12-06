8d1d1d17986093d860d8df118a250a9ab16b3997
Igor Vladimir <igor.alencar@tcema.tc.br>
2024-12-06 12:23:22 -0300

    fix(Depoimento): add atributos faltantes do mapeamento

--- src/main/java/com/ufma/portalegressos/database/entities/DepoimentoEntity.java
+++ src/main/java/com/ufma/portalegressos/database/entities/DepoimentoEntity.java
@@ -2,18 +2,25 @@ package com.ufma.portalegressos.database.entities;
 
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
 import lombok.*;
+import java.time.LocalDate;
 
 @Entity
 @Getter
 @Setter
+@Builder
 @AllArgsConstructor
 @NoArgsConstructor
 @Table(name="depoimento")
 public class DepoimentoEntity {
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer idDepoimento;
+    private String texto;
+    private LocalDate data;
     @ManyToOne
     @JoinColumn(name="id_egresso", nullable=false)
+    @NotNull
     private EgressoEntity egresso;
 }
