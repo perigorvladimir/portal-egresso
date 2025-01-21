7cf7c08d878e2472cf2b95f3646ef68477efb620
Igor Alencar <igorvcalencar@gmail.com>
2025-01-21 18:48:02 -0300

    Add campo Enum TipoNivel em Curso

---
+++ src/main/java/com/ufma/portalegresso/application/domain/TipoAreaTrabalho.java
@@ -0,0 +1,11 @@
+package com.ufma.portalegresso.application.domain;
+
+public enum TipoAreaTrabalho {
+    FINANCEIRO,
+    EDUCACAO,
+    TECNOLOGIA,
+    SAUDE,
+    RH,
+    VENDAS,
+    MARKETING
+}
---
+++ src/main/java/com/ufma/portalegresso/application/domain/TipoNivel.java
@@ -0,0 +1,9 @@
+package com.ufma.portalegresso.application.domain;
+
+public enum TipoNivel {
+    TECNOLOGO,
+    GRADUACAO,
+    POS_GRADUACAO,
+    MESTRADO,
+    DOUTORADO
+}
